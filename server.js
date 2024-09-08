0339273f00412f40eff245e045e282662dd4c17f
Tanjil <tanjilkh@gmail.com>
2024-09-08 12:20:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const foodRoute = require('./routes/foodRoute')
 const port = 3000
 
+app.use('/api/foods', foodRoute)
 
 app.get('/', (req,res) => {
     res.send('Running!')
