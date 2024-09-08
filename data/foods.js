f2d4ab0a3cec7c4745fa0f673ac7a1cda016454e
Tanjil <tanjilkh@gmail.com>
2024-09-08 13:09:39 +0100

    Create foods.js

---
+++ data/foods.js
@@ -0,0 +1,31 @@
+const foods = [
+    {
+        id: 1,
+        name: 'Continente Equilíbrio Leite Magro',
+        unit: 'ml',
+        serving_size: 100,
+        fat: 0.5,
+        satured_fat: 0.1,
+        carbohydrates: 4.8,
+        sugar: 4.8,
+        fiber: 0,
+        protein: 3.8,
+        salt: 0,
+    },
+
+    {
+        id: 2,
+        name: 'Continente Cereais Arroz Tufado com Chocolate',
+        unit: 'g',
+        serving_size: 100,
+        fat: 3,
+        satured_fat: 1.2,
+        carbohydrates: 79,
+        sugar: 26,
+        fiber: 4.2,
+        protein: 7.5,
+        salt: 0.65,
+    }
+]
+
+module.exports = foods
