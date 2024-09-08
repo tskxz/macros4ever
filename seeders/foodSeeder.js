a18156a386beaf0a2909a2e01de28d2452cf8994
Tanjil <tanjilkh@gmail.com>
2024-09-08 13:58:45 +0100

    Create foodSeeder.js

---
+++ seeders/foodSeeder.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const foods = require('../data/foods');
+const Food = require('../models/foodModel');
+
+mongoose.connect('mongodb://127.0.0.1:27017/food4ever');
+
+const importData = async () => {
+    const created_foods = await Food.create(foods);
+    console.log(`Created ${created_foods.length} foods.`);
+    process.exit();
+}
+
+importData();
