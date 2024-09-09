b8f166f5b7972505d7a41b81502872d17ec3de18
Tanjil <tanjilkh@gmail.com>
2024-09-09 11:10:40 +0100

    Reapply "Data - Foods"
    This reverts commit 00ca18e1c7f225a0957717e1b0dfbf9cbc43da03.

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const foods = require('../data/foods')
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json('foods')
+    res.json(foods)
 })
 
 module.exports = router
