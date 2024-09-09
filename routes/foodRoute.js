00ca18e1c7f225a0957717e1b0dfbf9cbc43da03
Tanjil <tanjilkh@gmail.com>
2024-09-09 10:46:46 +0100

    Revert "Data - Foods"

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,10 +1,8 @@
 const express = require('express')
-const foods = require('../data/foods')
-
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json(foods)
+    res.json('foods')
 })
 
 module.exports = router
