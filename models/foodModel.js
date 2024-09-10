d3cba9adf8ef826ab3bb171e730e9d8514eea2a4
Tanjil <tanjilkh@gmail.com>
2024-09-10 12:10:03 +0100

    Update foodModel.js
    add relationship with user

--- models/foodModel.js
+++ models/foodModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/macros4ever');
 
 const foodSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     name: String,
     unit: String,
     serving_size: Number,
