ff2ab2149872f78d949f8e0a5ae4f282e1b45292
Tanjil <tanjilkh@gmail.com>
2024-09-10 13:21:52 +0100

    Update foodRoute.js
    Each user can post their own food

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // @GET /api/foods
 // Public: Get all food items
 router.get('/', async (req, res) => {
-    const foods = await Food.find();
+    const foods = await Food.find({public: true});
     res.json(foods);
 });
 
@@ -17,16 +17,7 @@ router.get('/myfoods', authenticateToken, async (req, res) => {
     res.json(myFoods);
 });
 
-router.get('/:id', async(req, res) => {
-    const food = await Food.findById(req.params.id);
-    if(food) {
-        res.json(food);
-    } else {
-        res.status(404).json({ message: 'Food not found' });
-    }
-})
-
-router.post('/', authenticateToken, admin, async (req, res) => {
+router.post('/myfoods', authenticateToken, async (req, res) => {
     const {name, unit, serving_size, fat, satured_fat, carbohydrates, sugar, fiber, protein, salt, calories} = req.body;
     if(!name ||!fat || !carbohydrates || !protein){
         return res.status(400).json({ message: 'Missing name, fat, carbohydrates or protein fields' });
@@ -44,6 +35,41 @@ router.post('/', authenticateToken, admin, async (req, res) => {
         protein: protein,
         salt: salt,
         calories: calories || fat * 9 + carbohydrates * 4 + protein * 4,
+        public: false,
+    })
+    const createdFood = await food.save();
+    res.json(createdFood);
+   }
+})
+
+router.get('/:id', async(req, res) => {
+    const food = await Food.findById(req.params.id);
+    if(food) {
+        res.json(food);
+    } else {
+        res.status(404).json({ message: 'Food not found' });
+    }
+})
+
+router.post('/', authenticateToken, admin, async (req, res) => {
+    const {name, unit, serving_size, fat, satured_fat, carbohydrates, sugar, fiber, protein, salt, calories, public} = req.body;
+    if(!name ||!fat || !carbohydrates || !protein){
+        return res.status(400).json({ message: 'Missing name, fat, carbohydrates or protein fields' });
+   } else {
+    const food = new Food({
+        user: req.user._id,
+        name: name,
+        unit: unit,
+        serving_size: serving_size,
+        fat: fat,
+        satured_fat: satured_fat,
+        carbohydrates: carbohydrates,
+        sugar: sugar,
+        fiber: fiber,
+        protein: protein,
+        salt: salt,
+        calories: calories || fat * 9 + carbohydrates * 4 + protein * 4,
+        public: public || false,
     })
     const createdFood = await food.save();
     res.json(createdFood);
