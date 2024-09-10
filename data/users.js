50329d20a595e4d9ae892f256bee2e0e3cde8af6
Tanjil <tanjilkh@gmail.com>
2024-09-10 12:09:38 +0100

    Update users.js
    move admin user to first row

--- data/users.js
+++ data/users.js
@@ -1,17 +1,17 @@
 const users = [
     {
+        name: 'admin',
+        email: 'admin@example.com',
+        password: 'password123',
+        isAdmin: true,
+    },
+
+    {
         name: 'user1',
         email: 'user1@example.com',
         password: 'password123',
         isAdmin: false,
     },
-
-    {
-        name: 'admin',
-        email: 'admin@example.com',
-        password: 'password123',
-        isAdmin: true,
-    }
 ]
 
 module.exports = users
