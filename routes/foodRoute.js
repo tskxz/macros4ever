8dc1695be83cf3323549ac5c72ee0539eae227a5
Tanjil <tanjilkh@gmail.com>
2024-09-10 11:10:43 +0100

    Update foodRoute.js
    update food by id

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -36,4 +36,27 @@ router.post('/', authenticateToken, admin, async (req, res) => {
    }
 })
 
+router.put('/:id', authenticateToken, admin, async (req, res) => {
+    const {name, unit, serving_size, fat, satured_fat, carbohydrates, sugar, fiber, protein, salt, calories} = req.body;
+    const food = await Food.findById(req.params.id)
+    if(food){
+        food.name = name;
+        food.unit = unit;
+        food.serving_size = serving_size;
+        food.fat = fat;
+        food.satured_fat = satured_fat;
+        food.carbohydrates = carbohydrates;
+        food.sugar = sugar;
+        food.fiber = fiber;
+        food.protein = protein;
+        food.salt = salt;
+        food.calories = calories;
+
+        const updatedFood = await food.save();
+        res.json(updatedFood);
+    } else {
+        res.status(404).json({ message: 'Food not found' });
+    }
+})
+
 module.exports = router;
