995e7a8b49dccd18b855d40413a368dbc743e5fb
Tanjil <tanjilkh@gmail.com>
2024-09-13 11:23:15 +0100

    Update mealRoute.js

--- routes/mealRoute.js
+++ routes/mealRoute.js
@@ -2,21 +2,13 @@ const express = require('express');
 const Meal = require('../models/mealModel');
 const Food = require('../models/foodModel');
 const authenticateToken = require('../middleware/authMiddleware');
-const { getMeals, createMeal } = require('../controllers/mealController');
+const { getMeals, createMeal, deleteMeal } = require('../controllers/mealController');
 
 const router = express.Router()
 
 router.get('/', authenticateToken, getMeals);
 router.post('/', authenticateToken, createMeal);
 
-router.delete('/:id', authenticateToken, async (req, res) => {
-    const meal = await Meal.findByIdAndDelete(req.params.id)
-    if(meal){
-        await meal.deleteOne({_id: meal._id});
-        res.json({ message: 'Meal deleted successfully' });
-    } else {
-        res.status(404).json({ message: 'Meal not found' });
-    }
-})
+router.delete('/:id', authenticateToken, deleteMeal)
 
 module.exports = router
