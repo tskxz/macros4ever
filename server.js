92b599abcae15f5d9775db90500d07dfdf7953fe
Tanjil <tanjilkh@gmail.com>
2024-09-13 14:33:38 +0100

    Update server.js
    describe routes

--- server.js
+++ server.js
@@ -16,8 +16,65 @@ app.use('/api/users', userRoute);
 app.use('/api/meals', mealRoute);
 
 app.get('/', (req, res) => {
-    res.send('Running!');
-});
+    res.json({
+      message: "Welcome to the macros4ever API!",
+      routes: {
+        foodRoutes: {
+          getAllFoods: {
+            method: "GET",
+            route: "/api/foods",
+            description: "Retrieve a list of all available food items."
+          },
+          getMyFoods: {
+            method: "GET",
+            route: "/api/foods/myfoods",
+            description: "Retrieve all food items created by the authenticated user."
+          },
+          createMyFood: {
+            method: "POST",
+            route: "/api/foods/myfoods",
+            description: "Create a new food item for the authenticated user."
+          },
+
+        },
+        mealRoutes: {
+          getAllMeals: {
+            method: "GET",
+            route: "/api/meals",
+            description: "Retrieve all meals created by the authenticated user."
+          },
+          getMealById: {
+            method: "GET",
+            route: "/api/meals/:id",
+            description: "Retrieve a single meal created by the authenticated user."
+          },
+          createMeal: {
+            method: "POST",
+            route: "/api/meals",
+            description: "Create a new meal for the authenticated user."
+          },
+          deleteMealById: {
+            method: "DELETE",
+            route: "/api/meals/:id",
+            description: "Delete a meal created by the authenticated user."
+          }
+        },
+        userRoutes: {
+          loginUser: {
+            method: "POST",
+            route: "/api/users/login",
+            description: "Log in a user and receive an authentication token."
+          },
+          registerUser: {
+            method: "POST",
+            route: "/api/users/register",
+            description: "Register a new user."
+          },
+        }
+      }
+    });
+  });
+  
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
